3418467100cdd6d0a4204d9e0749620bf889f2e8
kohei-tofu <fukouhei001@gmail.com>
2021-06-22 17:43:03 +0900

    update personANDothers2

--- dataset/coco_selection.py
+++ dataset/coco_selection.py
@@ -183,51 +183,6 @@ def func_person(coco):
     return __ret_img, __map_catID, __map_invcatID, __new_cat_list
 
 
-def func_personANDothers2(coco):
-    
-    __ret_img = []
-    __map_catID = {}
-    __map_invcatID = {}
-    __new_cat_list = list()
-
-    __ret_img = coco.getImgIds()
-    cats = coco.loadCats(coco.getCatIds())
-    nms = [cat['name'] for cat in cats]
-    _loop = 0
-
-    for cat in nms:
-
-        catIds = coco.getCatIds(catNms=cat)
-        if cat == 'person':
-            __map_catID[int(catIds[-1])] = 0
-            __map_invcatID[0] = int(catIds[-1])
-            _loop += 1
-            cat_element = list(filter(lambda cat_loop: cat_loop['name'] == cat, coco.dataset['categories']))
-            __new_cat_list.append(cat_element[0])
-        else:
-            __map_catID[int(catIds[-1])] = _loop
-            __map_invcatID[1] = 2
-
-    #cat_element = {'supercategory': 'person', 'id': 1, 'name': 'person'}
-    cat_element = {'supercategory': 'others', 'id': 2, 'name': 'others'}
-    __new_cat_list.append(cat_element)
-
-    __map_catID["id"] = "img"
-    return __ret_img, __map_catID, __map_invcatID, __new_cat_list
-
-
-def func_personANDothers2_commercial(coco):
-    
-    __ret_img = []
-    img_ids, __map_catID, __map_invcatID, __new_cat_list = func_personANDothers2(coco)
-
-    for _id in img_ids:
-        id_license = coco.imgs[_id]['license']
-        if id_license >= 4:
-            __ret_img.append(_id)
-    __map_catID["id"] = "img"
-    return __ret_img, __map_catID, __map_invcatID, __new_cat_list
-
 def func_personANDothers(coco):
     __ret_img = []
     __map_catID = {}
@@ -247,5 +202,54 @@ def func_personANDothers(coco):
     __map_catID["id"] = "img"
     return __ret_img, __map_catID, __map_invcatID, __new_cat_list
 
+
+def func_personANDothers2(coco):
+    
+    __ret_img = []
+    __map_catID = {}
+    __map_invcatID = {}
+    __new_cat_list = list()
+
+    __ret_img = coco.getImgIds()
+    cats = coco.loadCats(coco.getCatIds())
+    nms = [cat['name'] for cat in cats]
+    _loop = 0
+
+    for cat in nms:
+
+        catIds = coco.getCatIds(catNms=cat)
+        if cat == 'person':
+            __map_catID[int(catIds[-1])] = 0
+            __map_invcatID[0] = int(catIds[-1])
+            _loop += 1
+
+            cat_element = list(filter(lambda cat_loop: cat_loop['name'] == cat, coco.dataset['categories']))
+            __new_cat_list.append(cat_element[0])
+        else:
+            __map_catID[int(catIds[-1])] = _loop
+            __map_invcatID[1] = 2
+
+    #cat_element = {'supercategory': 'person', 'id': 1, 'name': 'person'}
+    cat_element = {'supercategory': 'others', 'id': 2, 'name': 'others'}
+    __new_cat_list.append(cat_element)
+    print(__map_catID)
+    print(__map_invcatID)
+    print(new_cat_list)
+
+    __map_catID["id"] = "img"
+    return __ret_img, __map_catID, __map_invcatID, __new_cat_list
+
+
+def func_personANDothers2_commercial(coco):
+    
+    __ret_img = []
+    img_ids, __map_catID, __map_invcatID, __new_cat_list = func_personANDothers2(coco)
+
+    for _id in img_ids:
+        id_license = coco.imgs[_id]['license']
+        if id_license >= 4:
+            __ret_img.append(_id)
+    __map_catID["id"] = "img"
+    return __ret_img, __map_catID, __map_invcatID, __new_cat_list
 
 
